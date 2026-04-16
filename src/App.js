31074ad494bcb8300cdae901266e11ff630bf89c
Giga <243737693+giga0023@users.noreply.github.com>
2026-04-16 18:19:18 +1000

    Initial commit: project structure setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch("https://corsproxy.io/?url=https://www.reddit.com/r/popular.json", {
+      headers: { "User-Agent": "reddit-light-app" }
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Reddit Light</h1>
     </div>
   );
 }
