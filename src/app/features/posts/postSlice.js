727713061653a1da38dace5a13b8cff1e5387cd1
Giga <243737693+giga0023@users.noreply.github.com>
2026-04-17 11:40:43 +1000

    Add posts slice with fetchPosts thunk and connect Redux store

---
+++ src/app/features/posts/postSlice.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+    posts: [],
+    isLoading: false,
+    hasError: false
+};
+
+const fetchPosts = createAsyncThunk(
+    'posts/fetchPosts',
+    async () => {
+        const response = await fetch('https://www.reddit.com/r/popular.json');
+        const json = await response.json();
+        return json;
+    }
+);
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState: initialState,
+    reducers: {
+        addPost: (state, action) => {
+            state.posts.push(action.payload)
+        },
+        updatesUpvotes: (state, action) => {
+            const post = state.posts.find(p => p.id === action.payload.id)
+            if (post) {
+               post.ups++
+            } 
+        }
+    },
+    extraReducers : builder => {
+        builder
+          .addCase(fetchPosts.pending, (state) => {
+            state.isLoading = true
+            state.hasError = false
+          })
+          .addCase(fetchPosts.fulfilled, (state, action) => {
+            state.posts = action.payload.data.children.map(child => child.data)
+            state.isLoading = false
+            state.hasError = false
+          })
+          .addCase(fetchPosts.rejected, (state) => {
+            state.isLoading = false
+            state.hasError = true
+          })
+    }
+});
+
+export const { addPost, updatesUpvotes } = postsSlice.actions;
+export { fetchPosts };
+export default postsSlice.reducer;
