9486d302e0ff587afa59caeb00ad381a2ccb788b
Giga <243737693+giga0023@users.noreply.github.com>
2026-04-17 10:44:23 +1000

    Define data types: Post, User, Comment entities

---
+++ src/types/index.js
@@ -0,0 +1,29 @@
+export const Post = {
+    id: "qwerty2323",
+    title: "Pete Hegseth quotes fake",
+    author: "goteamnick",
+    subreddit: "politics",
+    created_utc: 1776297201,
+    ups: 19553,
+    downs: 0,
+    num_comments: 1133,
+    total_awards_received: 0,
+    thumbnail: "https://...",
+    url: "https://..."
+};
+
+export const User = {
+    id: "qwerty2323",
+    name: "goteamnick",
+    link_karma: 50000,
+    created_utc: 1776297201
+};
+
+export const Comment = {
+    id: "comment_id",
+    author: "username",
+    body: "Great post!",
+    created_utc: 1776297201,
+    ups: 45,
+    downs: 2
+};
