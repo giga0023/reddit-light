7ca3b69625262d6bbc19dbc87076fb093236dfdf
Giga <243737693+giga0023@users.noreply.github.com>
2026-04-18 16:50:00 +1000

    Add PostCard.css for styling

--- src/app/components/PostCard.jsx
+++ src/app/components/PostCard.jsx
@@ -1,13 +1,16 @@
+import './PostCard.css'
+
 export default function PostCard(props) {
     const post = props.post
-
     return (
-        <div>
-            <p className="author">{post.author}</p>
-            <h3 className="title">{post.title}</h3>
-            <img src={post.url} alt="post-image" />
-            <div className="upvotes">{post.ups}</div>
-            <div className="comments">{post.num_comments}</div>
+        <div className="post-card">
+            <p className="post-author">{post.author}</p>
+            <h3 className="post-title">{post.title}</h3>
+            <img src={post.url} alt="post-image" className="post-image"/>
+            <div className="post-stats">
+                <span className="upvotes">⬆️ {post.ups}</span>
+                <span className="comments">💬 {post.num_comments}</span>
+            </div>
         </div>
     )
 };
