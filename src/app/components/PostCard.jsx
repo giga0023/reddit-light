e391c3e606fd22b24c347945ab070e3bf5aee463
Giga <243737693+giga0023@users.noreply.github.com>
2026-04-18 14:36:49 +1000

    Add PostCard.jsx

---
+++ src/app/components/PostCard.jsx
@@ -0,0 +1,13 @@
+export default function PostCard(props) {
+    const post = props.post
+
+    return (
+        <div>
+            <p className="author">{post.author}</p>
+            <h3 className="title">{post.title}</h3>
+            <img src={post.thumbnail} alt="post-image" />
+            <div className="upvotes">{post.ups}</div>
+            <div className="comments">{post.num_comments}</div>
+        </div>
+    )
+};
